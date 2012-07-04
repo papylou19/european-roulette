159ea87b1e7934c4556dceec65894fc41618ac46
mvahe91@gmail.com <mvahe91@gmail.com@1db2f034-1ac9-5770-4c3c-caf94de7f5e6>
2012-07-04 14:37:40 +0000

    Starting Roulette project

---
+++ trunk/Roulette/Models/RouletteModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Roulette.Models
+{
+    public class ColorField
+    {
+        public int Number {get;set;}
+        public bool Color { get; set; }
+
+        public ColorField(int number, bool color)
+        {
+            this.Number = number;
+            this.Color = color;
+        }
+    }
+
+    public class TableModel
+    {
+        public ColorField[] Colors { get; set; }
+    }
+}
